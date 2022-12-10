ad8b5f0851ca1b0228703d6f7854e1382b253bbf
Mohamed Elweza <moahmed9899@gmail.com>
2022-12-10 06:44:32 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 2),);
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 3);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => secondPage()
+    )
+    );
+  }
+
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.forward();
+    return Scaffold(
+      body: Center(
+          child: FadeTransition(
+            opacity: _animation,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlutterLogo(size: 150,),
+            ),
+          ),
+        ),
+    );
+  }
+}
+
+class secondPage extends StatelessWidget {
+  const secondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second page'), leading: IconButton(onPressed: (){Navigator.of(context).pop();}, icon: Icon(Icons.arrow_back)),),
+      body: null,
+    );
+  }
+}
+
+
